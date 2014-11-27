5ed8a3609e84bb3672b0b2f5b8da2a8bd5fc7113
Stephan Grein <stephan.grein@gcsc.uni-frankfurt.de>
2014-11-27 12:23:56 +0100

    Added template project.

--- src/main/java/edu/gcsc/vrl/MembranePotentialMapping/MembranePotentialMappingPluginConfigurator.java
+++ src/main/java/edu/gcsc/vrl/MembranePotentialMapping/MembranePotentialMappingPluginConfigurator.java
@@ -10,15 +10,24 @@ import edu.gcsc.vrl.MembranePotentialMapping.types.SectionArrayType;
 import edu.gcsc.vrl.MembranePotentialMapping.types.SectionType;
 import edu.gcsc.vrl.MembranePotentialMapping.userdata.Clamp;
 import edu.gcsc.vrl.MembranePotentialMapping.userdata.Section;
+import eu.mihosoft.vrl.io.IOUtil;
+import eu.mihosoft.vrl.io.VJarUtil;
+import eu.mihosoft.vrl.lang.visual.CompletionUtil;
 import eu.mihosoft.vrl.system.InitPluginAPI;
 import eu.mihosoft.vrl.system.PluginAPI;
 import eu.mihosoft.vrl.system.PluginDependency;
 import eu.mihosoft.vrl.system.PluginIdentifier;
+import eu.mihosoft.vrl.system.ProjectTemplate;
 import eu.mihosoft.vrl.system.VPluginAPI;
 import eu.mihosoft.vrl.system.VPluginConfigurator;
+import eu.mihosoft.vrl.system.VRLPlugin;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 /**
@@ -26,6 +35,7 @@ import javax.imageio.ImageIO;
  * @author sgrein adapted by mbreit
  */
 public class MembranePotentialMappingPluginConfigurator extends VPluginConfigurator{
+	private File templateProjectSrc;
 
     public MembranePotentialMappingPluginConfigurator() {
         //specify the plugin name and version
@@ -104,8 +114,66 @@ public class MembranePotentialMappingPluginConfigurator extends VPluginConfigura
        // nothing to unregister
    }
 
-    @Override
-    public void init(InitPluginAPI iApi) {
-       // nothing to init
-   }
+    
+ @Override
+    public void install(InitPluginAPI iApi) {
+        // ensure template projects are updated
+        new File(iApi.getResourceFolder(), "template-01.vrlp").delete();
+    }
+	
+
+	@Override
+ 	   public void init(InitPluginAPI iApi) {
+
+        CompletionUtil.registerClassesFromJar(
+                VJarUtil.getClassLocation(MembranePotentialMappingPluginConfigurator.class));
+        
+        initTemplateProject(iApi);
+    }
+    
+	
+	private void initTemplateProject(InitPluginAPI iApi) {
+        templateProjectSrc = new File(iApi.getResourceFolder(), "template-01.vrlp");
+
+        if (!templateProjectSrc.exists()) {
+            saveProjectTemplate();
+        }
+
+        iApi.addProjectTemplate(new ProjectTemplate() {
+
+            @Override
+            public String getName() {
+                return "MPM Plugin - Template 1";
+            }
+
+            @Override
+            public File getSource() {
+                return templateProjectSrc;
+            }
+
+            @Override
+            public String getDescription() {
+                return "MPM Plugin Template Project 1";
+            }
+
+            @Override
+            public BufferedImage getIcon() {
+                return null;
+            }
+        });
+	}
+	
+	 private void saveProjectTemplate() {
+        InputStream in = MembranePotentialMappingPluginConfigurator.class.getResourceAsStream(
+                "/edu/gcsc/vrl/MembranePotentialMapping/resources/projects/template-01.vrlp");
+        try {
+            IOUtil.saveStreamToFile(in, templateProjectSrc);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(VRLPlugin.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(VRLPlugin.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+    }
  }
